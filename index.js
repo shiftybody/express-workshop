2d4b23f33bdcebcd5ddf27b4d41e75fcd9f8667f
shiftybody <shiftybody@gmail.com>
2023-09-04 11:22:56 -0600

    some rutes created for pokedex file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const { pokemon } = require('./pokedex.json');
 
 /**
  * Verbos HTTP
@@ -8,14 +9,18 @@ const app = express();
 
 app.get('/', (req, res, next) => {
     res.status(200);
-    res.send('<h1>Bienvenido</h1>');
+    res.send("Bienvenido al Pokedex");
 });
 
-app.get('/:name', (req, res, next) => {
-    let name = req.params.name
+app.get('/pokemon', (req,res,next) => {
     res.status(200);
-    res.send(`<h1>Hola ${name}</h1>`)
-})
+    res.send(pokemon);
+});
+
+app.get('/pokemon/:id', (req, res, next) => {
+    res.status(200);
+    res.send(pokemon[req.params.id - 1]);
+});
 
 app.listen(process.env.port || 3000, () => {
     console.log('Server is running ...');
