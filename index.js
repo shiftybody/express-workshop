c5dcf8a5c0872f90b04c6cb8e57fcc4b2b6b0047
shiftybody <shiftybody@gmail.com>
2023-09-04 11:04:19 -0600

    added nodemon dependency

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express'); 
+const express = require('express');
 const app = express();
 
 /**
@@ -8,12 +8,16 @@ const app = express();
 
 app.get('/', (req, res, next) => {
     res.status(200);
-    res.send('<h1>Bienvenido<h1>');
+    res.send('<h1>Bienvenido</h1>');
 });
 
-app.listen(3000, () => {
-    console.log(`
-    Server is running on port: 3000`
-    );
+app.get('/:name', (req, res, next) => {
+    let name = req.params.name
+    res.status(200);
+    res.send(`<h1>Hola ${name}</h1>`)
+})
+
+app.listen(process.env.port || 3000, () => {
+    console.log('Server is running ...');
 });
 
